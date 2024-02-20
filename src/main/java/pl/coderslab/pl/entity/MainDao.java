e84389d17d56c1d5622ba1b56666ca19014c4c28
twój login do GitHuba <kowina@gazeta.pl>
2024-02-20 16:34:02 +0100

    gotowe create

--- src/main/java/pl/coderslab/pl/entity/MainDao.java
+++ src/main/java/pl/coderslab/pl/entity/MainDao.java
@@ -7,9 +7,14 @@ public class MainDao {
     public static void main(String[] args) {
      //User user = new User("Mateusz", "kowina@gazeta.pl", "paswword1");
      //User user1 = new User("Tomasz", "codso@wp.pl", "pass2");
-        User user = new User("Witold", "dsff@jh","pass2");
+      //  User user = new User("Witold", "f@gjjjh","pass2");
+        User user = new User();
+        user.setUserName("Kuba");
+        user.setEmail("sdsdh@hgj");
+        user.setPassword("asdfg");
         System.out.println(user.toString());
         UserDao userDao = new UserDao();
+
         userDao.create(user);
         System.out.println(user.toString());
     }
