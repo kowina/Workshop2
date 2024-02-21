2a203e981b8a11eabc343e9cb312ab0b8d3f8d5a
twój login do GitHuba <kowina@gazeta.pl>
2024-02-21 15:05:09 +0100

    find all gotowe

--- src/main/java/pl/coderslab/pl/entity/MainDao.java
+++ src/main/java/pl/coderslab/pl/entity/MainDao.java
@@ -2,28 +2,33 @@ package pl.coderslab.pl.entity;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Arrays;
 
 public class MainDao {
     public static void main(String[] args) {
-     //User user = new User("Mateusz", "kowina@gazeta.pl", "paswword1");
-     //User user1 = new User("Tomasz", "codso@wp.pl", "pass2");
-      //  User user = new User("Witold", "f@gjjjh","pass2");
-       // User user = new User();
-       // user.setUserName("Kuba");
-      // user.setEmail("sdsdh@hgj");
-      //  user.setPassword("asdfg");
-       // System.out.println(user.toString());
+        //User user = new User("Mateusz", "er@ger.pl", "paswword1");
+        //User user1 = new User("Tomasz", "codso@wp.pl", "pass2");
+        //User user3 = new User("Witold", "fdfghj@gjjjh","passfg");
         UserDao userDao = new UserDao();
-       // User user = userDao.read(1);
-       // System.out.println(user);
-       // user.setUserName("Leon");
-       // user.setEmail("jhgfd");
-       // user.setPassword("kjhgfd");
-       // userDao.update(user);
+        //User user5 = new User();
+        //user5.setUserName("Olo");
+        //user5.setEmail("sdfhj@hgj");
+        //user5.setPassword("asdfg");
+        //userDao.create(user3);
+        //User user = userDao.read(6);
+        //System.out.println(user.toString());
+        //user.setUserName("Leon");
+        //user.setEmail("jhgfd");
+        //user.setPassword("kjhgfd");
+        //userDao.update(user);
         //userDao.read(22);
         //User user = userDao.read(2);
         //System.out.println(userDao.read(4).toString());
-       // System.out.println(userDao.read(24));
-        userDao.delete(18);
+        // System.out.println(userDao.read(24));
+        //userDao.delete(7);
+
+        userDao.findAll();
+        userDao.readArray(userDao.findAll());
     }
+
 }
